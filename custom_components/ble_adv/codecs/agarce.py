002af2e8f445f4e368f84d61932d8ad7c2ead94a
NicoIIT <nicolas.clercrenaud@gmail.com>
2025-05-24 08:08:44 +0000

    Add missing sub_type for reverse agarce fan

--- custom_components/ble_adv/codecs/agarce.py
+++ custom_components/ble_adv/codecs/agarce.py
@@ -76,6 +76,7 @@ class AgarceEncoder(BleAdvCodec):
             if enc_cmd.arg2 & 0x10:
                 attr_chg.append(ATTR_OSC)
             attrs = {
+                ATTR_SUB_TYPE: FAN_TYPE_6SPEED,
                 ATTR_SPEED: enc_cmd.arg0 & 0x0F,
                 ATTR_ON: (enc_cmd.arg0 & 0x80) != 0,
                 ATTR_DIR: (enc_cmd.arg0 & 0x10) == 0,
