c745718cdbf4717497b46b037d27f9bd8011f5b2
NicoIIT <nicolas.clercrenaud@gmail.com>
2025-09-17 16:15:52 +0000

    Implement Device trigger "any_entity_state"

---
+++ tests/test_device_trigger.py
@@ -0,0 +1,19 @@
+"""Device Trigger tests."""
+
+# ruff: noqa: S101
+from unittest import mock
+
+from ble_adv import device_trigger
+from homeassistant.core import HomeAssistant
+
+
+async def test_list_trigger(hass: HomeAssistant) -> None:
+    """Test List Trigger."""
+    lst = await device_trigger.async_get_triggers(hass, "toto")
+    assert lst == [{"device_id": "toto", "domain": "ble_adv", "platform": "device", "type": "any_entity_state"}]
+
+
+async def test_attach_trigger(hass: HomeAssistant) -> None:
+    """Test attach trigger."""
+    conf = {"device_id": "toto", "domain": "ble_adv", "platform": "device", "type": "any_entity_state"}
+    await device_trigger.async_attach_trigger(hass, conf, mock.MagicMock(), mock.MagicMock())
